3f5efb559b65dff580b49150013cdaea1f295306
raxo17 <114686507+raxo17@users.noreply.github.com>
2024-11-16 16:20:42 +0100

    a

--- src/friends.py
+++ src/friends.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+import os
+from collect import collect_participants
+
 def friends(a,b):
-    """
-    if a.friends == b.id:
+    if b.id in a.friends:
         return True
     return False
-    """
 
 def bidirectional_friends(a,b)->bool:
     if friends(a,b) and friends(b,a):
         return True
     return False
 
+DATA_FILE = Path(os.path.dirname(__file__)).resolve().parent / "data" / "participants-prova.json"
+
+participants = collect_participants(DATA_FILE)
+for participant in participants:
+    participant.friend_registration
