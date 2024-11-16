d8ebd9d6cfcfa74770ffc8e76174da89ed85635f
Kuequasho <132204259+Kuequasho@users.noreply.github.com>
2024-11-16 15:32:47 +0100

    Update Assignació-Idiomes.py

--- Assignació-Idiomes.py
+++ Assignació-Idiomes.py
@@ -3,24 +3,25 @@ from yogi import *
 def assignacio_idiomes(L: list)->list[str]:
     #pongo la L por si acabamos usando una lista con los prefiles de la gente (lista de listas)
 
-    possible_language = [0, 0, 0, 0, 0]
+
+    language_speakers = [[],[],[],[],[]]
     possible_language_list = ['español', 'catala', 'english', 'francais', 'deutsch']
 
     for x in range(len(L)):
-        #Por si usamos la lista de listas, asignar la persona a x para ver sus datos individualmente
-        x = L[x]
-        print('perfil individual: ', x)
-        a = x[3]
-        print('lista de idiomas individual: ', a)
-        #a es el índice de idiomas que habla. De ser una lista de idiomas, hace falta volver a asignar una variable a los idiomas para poder acceder a ellos individualmente.
-            
-        for j in range(len(possible_language)):
-        #assignar la gent al idioma particular que parlen
-            for i in range(len(a)):
-                    if possible_language_list[j] == a[i]:
-                        possible_language[j] = possible_language[j] + 1
-                #añadir el id de la persona que habla es idioma j
-    return possible_language
+            #Por si usamos la lista de listas, asignar la persona a x para ver sus datos individualmente
+            x = L[x]
+            print('perfil individual: ', x)
+            a = x[3]
+            print('lista de idiomas individual: ', a)
+            #a es el índice de idiomas que habla. De ser una lista de idiomas, hace falta volver a asignar una variable a los idiomas para poder acceder a ellos individualmente.
+            id = x[0]
+            for j in range(len(language_speakers)):
+            #assignar la gent al idioma particular que parlen
+                for i in range(len(a)):
+                        if possible_language_list[j] == a[i]:
+                            language_speakers[j] += [id]
+                    #añadir el id de la persona que habla es idioma j
+    return language_speakers
 
     
 def creació_perfil()->list[str]:
