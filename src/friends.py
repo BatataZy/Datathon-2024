d48413dd02fabb7dd95e76bc797bc09059a85968
ProGabo <gabozalopez@gmail.com>
2024-11-16 20:50:01 +0100

    PENEEEEEEEE

--- src/friends.py
+++ src/friends.py
@@ -3,8 +3,8 @@ import os
 from collect import collect_participants
 from yogi import read
 
-def friends(a,b):
-    if b.id in a.friends:
+def friends(a,b) -> bool:
+    if b.participant in a.friend_registration:
         return True
     return False
  
